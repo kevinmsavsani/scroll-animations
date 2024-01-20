92ef34353937ff2c2b0ef9d8d448b1bc70027063
Kevin Savsani <kevinsavsani@Kevins-MacBook-Air.local>
2024-01-20 12:48:20 +0530

    updated readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Header from "./components/Header";
-import { motion, useScroll, useTransform } from "framer-motion";
 import SectionLayout from "./components/SectionLayout";
 import HorizontalWrapper from "./components/HorizontalWrapper";
 import Card from "./components/Card";
@@ -11,26 +10,12 @@ import TextSection from "./components/TextSection";
 import ZoomSection from "./components/ZoomSection";
 
 export default function Home() {
-  const video = React.useRef<HTMLDivElement>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: video,
-    offset: ["start end", "end start"],
-  });
-
-  const opacity = useTransform(scrollYProgress, [0, 0.65, 1], [1, 1, 0]);
-  const scale = useTransform(
-    scrollYProgress,
-    [0, 0.6, 0.8, 0.9],
-    [1, 0.8, 0.8, 0]
-  );
-
   return (
     <div className="z-10">
       <Header />
       <main className="w-full">
         <SectionLayout>
-          <HorizontalWrapper height="40rem" direction={-1400}>
+        <HorizontalWrapper height="40rem" direction={-1400}>
             <div className="grid grid-cols-5 gap-4">
               {cards.map((card, index) => (
                 <Card
@@ -65,14 +50,8 @@ export default function Home() {
           <TextSection />
         </SectionLayout>
 
-
-
         <SectionLayout>
           <ZoomSection />
-        </SectionLayout>
-
-        <SectionLayout>
-          <TextSection />
         </SectionLayout>
       </main>
     </div>
