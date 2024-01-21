8950bffbf396bd275feccbc81b80d35529993491
Kevin Savsani <kevinsavsani@Kevins-MacBook-Air.local>
2024-01-21 14:42:13 +0530

    added images

--- src/app/utils/cards.js
+++ src/app/utils/cards.js
@@ -4,76 +4,104 @@ import smoke_2 from "/public/images/monkey.png";
 import smoke_head from "/public/images/smoke_head.png";
 import smoke from "/public/images/smoke_2.png";
 import bitcoin from "/public/images/bitcoin.jpg";
+import tree from "/public/images/tree.png";
+import mountain from "/public/images/mountain.png";
+import pink_sky from "/public/images/pink_sky.png";
+import balloons from "/public/images/balloons.png";
+import sky from "/public/images/sky.png";
+import circle from "/public/images/circle.png";
+import sea from "/public/images/sea.png";
 
 export const cards = [
   {
     id: 1,
-    title: "Monkey King",
-    description: "A monkey king who is a disciple of Buddha.",
-    image: bitcoin,
-    link: "/monkey-king",
+    title: "Pink Tree",
+    description: "A Beautiful pink tree.",
+    image: tree,
+    link: "/pink-tree",
   },
   {
     id: 2,
-    title: "Bulb Tree",
-    description: "A tree that grows bulbs.",
-    image: bulb,
-    link: "/bulb-tree",
+    title: "Mountain",
+    description: "A Mountain with sea.",
+    image: mountain,
+    link: "/mountain",
   },
   {
     id: 3,
-    title: "Smoke",
-    description: "A smoke that is a byproduct of fire.",
-    image: smoke,
-    link: "/smoke",
+    title: "pink sky",
+    description: "A sky which is pink.",
+    image: pink_sky,
+    link: "/pink-sky",
   },
   {
     id: 4,
-    title: "Smoke Head",
-    description: "A smoke that is a byproduct of fire.",
-    image: smoke_head,
-    link: "/smoke-head",
+    title: "balloons",
+    description: "A sky with balloons.",
+    image: balloons,
+    link: "/balloons",
   },
   {
     id: 5,
-    title: "Smoke 2",
-    description: "A smoke that is a byproduct of fire.",
-    image: smoke_2,
-    link: "/smoke-2",
-  },
+    title: "sky",
+    description: "A beautiful sky",
+    image: sky,
+    link: "/sky",
+  },  
   {
     id: 6,
-    title: "Monkey King",
-    description: "A monkey king who is a disciple of Buddha.",
-    image: bitcoin,
-    link: "/monkey-king",
+    title: "circle",
+    description: "A sky with circle.",
+    image: circle,
+    link: "/circle",
   },
   {
     id: 7,
+    title: "sea",
+    description: "A beautiful sea",
+    image: sea,
+    link: "/sea",
+  },
+  {
+    id: 8,
+    title: "Monkey King",
+    description: "A monkey king who is a disciple of Buddha.",
+    image: monkey,
+    link: "/monkey-king",
+  },
+  {
+    id: 9,
     title: "Bulb Tree",
     description: "A tree that grows bulbs.",
     image: bulb,
     link: "/bulb-tree",
   },
   {
-    id: 8,
+    id: 10,
     title: "Smoke",
     description: "A smoke that is a byproduct of fire.",
     image: smoke,
     link: "/smoke",
   },
   {
-    id: 9,
+    id: 11,
     title: "Smoke Head",
     description: "A smoke that is a byproduct of fire.",
     image: smoke_head,
     link: "/smoke-head",
   },
   {
-    id: 10,
+    id: 12,
     title: "Smoke 2",
     description: "A smoke that is a byproduct of fire.",
     image: smoke_2,
     link: "/smoke-2",
+  },  
+  {
+    id: 13,
+    title: "Bitcoin",
+    description: "A bitcoin image.",
+    image: bitcoin,
+    link: "/bitcoin",
   },
 ];
